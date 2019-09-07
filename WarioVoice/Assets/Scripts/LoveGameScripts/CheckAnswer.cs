24f98a2001c432bd10366834ed13ccd1eaa67d8c
Sebastian Rueda <jrueda275@unab.edu.co>
2019-09-07 11:39:33 -0500

    Voice Implemented for LoveGame and DanceCrystals

---
+++ WarioVoice/Assets/Scripts/LoveGameScripts/CheckAnswer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckAnswer : CommandParser
+{
+    WordController wordControl;
+
+    private void Start()
+    {
+        wordControl = GetComponent<WordController>();
+    }
+
+    public override void parseCommand(string command)
+    {
+        wordControl.checkAnswer(command);
+    }
+}
