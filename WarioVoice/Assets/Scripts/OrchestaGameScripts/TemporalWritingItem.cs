41f221318849f55572f935f02200e2919131deeb
Sebastian Rueda <jrueda275@unab.edu.co>
2019-09-19 18:31:08 -0500

    Changes made in love game and orchesta so fabian could test it
    xd

---
+++ WarioVoice/Assets/Scripts/OrchestaGameScripts/TemporalWritingItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TemporalWritingItem : MonoBehaviour
+{
+    public InputField input;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getInstrument()
+    {
+        FindObjectOfType<PatternCheckOrchesta>().parseCommand(input.text);
+    }
+
+    public void getWord()
+    {
+        FindObjectOfType<CheckAnswer>().parseCommand(input.text);
+    }
+}
