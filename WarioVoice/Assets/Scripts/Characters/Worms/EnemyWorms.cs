b07bc2038887e8ee83a68bf42d40eb230c235a76
Kevin Castellanos <kcastellanos278@unab.edu.co>
2019-09-22 00:22:19 -0500

    tiro parabolico de los enemigos

--- WarioVoice/Assets/Scripts/Characters/Worms/EnemyWorms.cs
+++ WarioVoice/Assets/Scripts/Characters/Worms/EnemyWorms.cs
@@ -10,6 +10,7 @@ public class EnemyWorms : MonoBehaviour
     [SerializeField] protected Transform _positionShoot;
     [SerializeField] protected GameObject _myFather;
     [SerializeField] protected GameObject _turret;
+    private const float _SPEED = 12; 
     public int TimeToShoot { get => _timeToShoot; set => _timeToShoot = value; }
 
     public virtual void Start()
@@ -22,11 +23,22 @@ public class EnemyWorms : MonoBehaviour
     public void aimPlayer()
     {
         float angle;
+        Vector3 toTarget = _pointingGun.transform.position - _positionShoot.position;
 
-        Vector2 vector = _pointingGun.transform.position - transform.position;
-        angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+        // Set up the terms we need to solve the quadratic equations.
+        float gSquared = Physics.gravity.sqrMagnitude;
+        float b = _SPEED * _SPEED + Vector3.Dot(toTarget, Physics.gravity);
+        float discriminant = b * b - gSquared * toTarget.sqrMagnitude;
 
-        _turret.transform.eulerAngles = new Vector3(0,0,angle);
+        float T_lowEnergy = Mathf.Sqrt(Mathf.Sqrt(toTarget.sqrMagnitude * 4f / gSquared));
+
+        float T = T_lowEnergy;// choose T_max, T_min, or some T in-between like T_lowEnergy
+
+        //Convert from time-to-hit to a launch velocity:
+        Vector3 velocity = toTarget / T - Physics.gravity * T / 2f;
+
+        angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+        _turret.transform.localEulerAngles = new Vector3(0, 0, angle);
     }
 
     public virtual void prepareShoot()
@@ -35,19 +47,49 @@ public class EnemyWorms : MonoBehaviour
     }
 
     public virtual void shootPlayer()
-    {
+    { 
         GameObject aux;
         float angle;
         aux = Instantiate(_proyectile, _positionShoot.position, Quaternion.identity);
-
+       
         Vector2 vector = _pointingGun.transform.position - aux.transform.position;
-        angle = Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg;
+
+        Vector3 toTarget = _pointingGun.transform.position - aux.transform.position;
+
+        // Set up the terms we need to solve the quadratic equations.
+        float gSquared = Physics.gravity.sqrMagnitude;
+        float b = _SPEED * _SPEED + Vector3.Dot(toTarget, Physics.gravity);
+        float discriminant = b * b - gSquared * toTarget.sqrMagnitude;
+
+        // Check whether the target is reachable at max speed or less.
+        if (discriminant < 0)
+        {
+            // Target is too far away to hit at this speed.
+            // Abort, or fire at max speed in its general direction?
+            Debug.Log("nopuedo golpear al objetivo");
+        }
+
+        float discRoot = Mathf.Sqrt(discriminant);
+
+        // Highest shot with the given max speed:
+        float T_max = Mathf.Sqrt((b + discRoot) * 2f / gSquared);
+
+        // Most direct shot with the given max speed:
+        float T_min = Mathf.Sqrt((b - discRoot) * 2f / gSquared);
+
+        // Lowest-speed arc available:
+        float T_lowEnergy = Mathf.Sqrt(Mathf.Sqrt(toTarget.sqrMagnitude * 4f / gSquared));
+
+        float T = T_lowEnergy;// choose T_max, T_min, or some T in-between like T_lowEnergy
+
+        //Convert from time-to-hit to a launch velocity:
+        Vector3 velocity = toTarget / T - Physics.gravity * T / 2f;
+
+        // Apply the calculated velocity (do not use force, acceleration, or impulse modes)
+        aux.GetComponent<Rigidbody2D>().AddForce(velocity, ForceMode2D.Impulse);
+
+        angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
         aux.transform.localEulerAngles = new Vector3(0, 0, angle - 90);
-
-        Vector3 vec = _pointingGun.transform.position - aux.transform.position;
-        vec.Normalize();
-        aux.GetComponent<MoveForward>().Direction = vec;
-
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
