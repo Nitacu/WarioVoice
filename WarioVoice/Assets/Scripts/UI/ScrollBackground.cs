e5d9dafd47f879e02744fb02db67ea0b5b07801f
FabianDelgadoSe <fabian.delgadose@hotmail.es>
2019-10-01 16:13:50 -0500

    Backgrounddinamico

---
+++ WarioVoice/Assets/Scripts/UI/ScrollBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollBackground : MonoBehaviour
+{
+    Material material;
+
+    [SerializeField] float lerpVelocity;
+
+
+    void Start()
+    {
+        material = GetComponent<RawImage>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Vector2 offset = material.mainTextureOffset;
+        offset.x += Time.deltaTime / lerpVelocity;
+        offset.y += Time.deltaTime / lerpVelocity;
+
+        material.mainTextureOffset = offset;
+    }
+}
