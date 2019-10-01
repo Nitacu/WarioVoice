cd8ac17b37b5e6476055e5f21d28c60306b7d53e
Sebastian Rueda <jrueda275@unab.edu.co>
2019-10-01 01:11:31 -0500

    Added developermode orquesta and lovegame

---
+++ WarioVoice/Assets/Scripts/LoveGameScripts/DisableTestDeveloper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableTestDeveloper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!GameManager.GetInstance().DeveloperMode)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    
+}
