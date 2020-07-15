179e3a525d49034e4ff1b2f9caedcad1cb63708e
Sebastian@SDSharp <Sebastian@sdssharp.com>
2020-07-14 21:07:59 -0500

    Cambios sala tito

---
+++ WarioVoice/Assets/Scripts/Koala Room/BallDragNDrop.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BallDragNDrop : MonoBehaviour
+{
+    private bool _isDraggingBall = false;
+    private Vector3 _startingPosition = Vector3.zero;
+    [SerializeField] private Canvas myCanvas;
+    private bool _canBallBeDragged = true;
+    private bool _isMouseOnTito = false;
+
+    public bool IsDraggingBall { get => _isDraggingBall; set => _isDraggingBall = value; }
+    public bool CanBallBeDragged { get => _canBallBeDragged; set => _canBallBeDragged = value; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startingPosition = gameObject.transform.position;      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsDraggingBall)
+        {
+            Vector3 MousePosition = Input.mousePosition;
+            //gameObject.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(MousePosition.x, MousePosition.y, 0));
+            Vector3 pos;
+            RectTransformUtility.ScreenPointToWorldPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out pos);
+            gameObject.transform.position = pos;
+            Debug.Log(gameObject.transform.localPosition);
+        }
+    }
+
+    public void startDrag()
+    {
+        if (CanBallBeDragged)
+        {
+            IsDraggingBall = true;
+        }
+    }
+
+    public void endDrag()
+    {
+        if (CanBallBeDragged)
+        {
+            gameObject.transform.position = _startingPosition;
+            IsDraggingBall = false;
+            if (_isMouseOnTito)
+            {
+                FindObjectOfType<MoodActionsController>().playWithTito();
+            }
+        }
+    }
+
+    public void mouseOnTiTo()
+    {
+        _isMouseOnTito = true;
+    }
+
+    public void mouseOffTiTo()
+    {
+        _isMouseOnTito = false;
+    }
+}
--- WarioVoice/Assets/Scripts/Koala Room/MoodActionsController.cs
+++ WarioVoice/Assets/Scripts/Koala Room/MoodActionsController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MoodActionsController : MonoBehaviour
 {
@@ -21,10 +22,16 @@ public class MoodActionsController : MonoBehaviour
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _eatButton;
     [SerializeField] private Button _showerButton;
-    [SerializeField] private Text _playText;
-    [SerializeField] private Text _eatText;
-    [SerializeField] private Text _showerText;
+    [SerializeField] private TextMeshProUGUI _playText;
+    [SerializeField] private TextMeshProUGUI _eatText;
+    [SerializeField] private TextMeshProUGUI _showerText;
     [SerializeField] private List<GameObject> _titoLifes;
+    [Header("UI Clocks")]
+    [SerializeField] private Image _playClock;
+    [SerializeField] private Image _eatClock;
+    [SerializeField] private Image _showerClock;
+    [Header("RAIN PREFAB")]
+    [SerializeField] private GameObject _rainPrefab;
 
     private ulong _lastPlayAction;
     private ulong _lastShowerAction;
@@ -123,11 +130,21 @@ public class MoodActionsController : MonoBehaviour
 
         if (!_playButton.IsInteractable())
         {
-            _playText.text = getTimeLeft(ENUM_Actions.PLAY);
+            _playText.text = getTimeLeft(ENUM_Actions.PLAY);           
+            _playClock.fillAmount = (getSecondsLeft(ENUM_Actions.PLAY) / _playSecondsToWait);
+            if (_playClock.fillAmount < 0)
+            {
+                _playClock.fillAmount = 0;
+            }
+            if (_playClock.fillAmount > 1)
+            {
+                _playClock.fillAmount = 1;
+            }
 
             if (isPlayActionReady())
             {
                 _playButton.interactable = true;
+                FindObjectOfType<BallDragNDrop>().CanBallBeDragged = true;
                 _playText.text = "";
             }
         }
@@ -135,6 +152,16 @@ public class MoodActionsController : MonoBehaviour
         if (!_eatButton.IsInteractable())
         {
             _eatText.text = getTimeLeft(ENUM_Actions.EAT);
+            _eatClock.fillAmount = getSecondsLeft(ENUM_Actions.EAT) / _eatSecondsToWait;
+
+            if (_eatClock.fillAmount < 0)
+            {
+                _eatClock.fillAmount = 0;
+            }
+            if (_eatClock.fillAmount > 1)
+            {
+                _eatClock.fillAmount = 1;
+            }
 
             if (isEatActionReady())
             {
@@ -146,6 +173,16 @@ public class MoodActionsController : MonoBehaviour
         if (!_showerButton.IsInteractable())
         {
             _showerText.text = getTimeLeft(ENUM_Actions.SHOWER);
+            _showerClock.fillAmount = getSecondsLeft(ENUM_Actions.SHOWER) / _showerSecondsToWait;
+
+            if (_showerClock.fillAmount < 0)
+            {
+                _showerClock.fillAmount = 0;
+            }
+            if (_showerClock.fillAmount > 1)
+            {
+                _showerClock.fillAmount = 1;
+            }
 
             if (isShowerActionReady())
             {
@@ -204,19 +241,76 @@ public class MoodActionsController : MonoBehaviour
         float secondsLeft = (float)((secondsToWait * 1000) - miliseconds) / 1000;
 
         string timer = "";
+
+        bool isHours = false;
         //Hours
         timer += ((int)secondsLeft / 3600).ToString() + "H ";
+        if (((int)secondsLeft / 3600) > 0)
+        {
+            isHours = true;
+            timer = ((int)secondsLeft / 3600).ToString() + "H";
+        }
         secondsLeft -= ((int)secondsLeft / 3600) * 3600;
         //minutes
-        timer += ((int)secondsLeft / 60).ToString("00") + "M ";
+        //timer += ((int)secondsLeft / 60).ToString("00") + "M ";
         //seconds
-        timer += (secondsLeft % 60).ToString("00") + "S";
+        //timer += (secondsLeft % 60).ToString("00") + "S";
+
+        if (!isHours)
+        {
+            timer = (secondsLeft % 60).ToString("00") + "S";
+
+            if (((int)secondsLeft / 60) > 0)
+            {
+                timer = ((int)secondsLeft / 60).ToString("00") + "M";
+            }
+        }
+        
+
         return timer;
+    }
+
+    private float getSecondsLeft(ENUM_Actions action)
+    {
+        ulong lastAction = 0;
+        float secondsToWait = 0;
+
+        switch (action)
+        {
+            case ENUM_Actions.PLAY:
+                lastAction = _lastPlayAction;
+                secondsToWait = _playSecondsToWait;
+                break;
+            case ENUM_Actions.SHOWER:
+                lastAction = _lastShowerAction;
+                secondsToWait = _showerSecondsToWait;
+                break;
+            case ENUM_Actions.EAT:
+                lastAction = _lastEatAction;
+                secondsToWait = _eatSecondsToWait;
+                break;
+        }
+
+        //timer
+        ulong diff = ((ulong)System.DateTime.Now.Ticks - lastAction);
+        ulong miliseconds = diff / System.TimeSpan.TicksPerMillisecond;
+        float secondsLeft = (float)((secondsToWait * 1000) - miliseconds) / 1000;
+
+        float extraSeconds = 0;
+        //Hours check
+        if (((int)secondsLeft / 3600) > 0)
+        {
+            extraSeconds = 3600 * ((int)secondsLeft / 3600);
+        }
+        secondsLeft -= (((int)secondsLeft / 3600) * 3600);
+        secondsLeft += extraSeconds;
+        return secondsLeft;
     }
 
     #region button click actions
     public void playWithTito()
     {
+        FindObjectOfType<BallDragNDrop>().CanBallBeDragged = false;
         _lastPlayAction = (ulong)System.DateTime.Now.Ticks;
         PlayerPrefs.SetString(LAST_PLAY_KEY, _lastPlayAction.ToString());
         _titoMood.addMoodPoints(_playPoints);
@@ -231,6 +325,10 @@ public class MoodActionsController : MonoBehaviour
     }
     public void showerWithTito()
     {
+        Camera cam = Camera.main;
+        Vector3 position = cam.ScreenToWorldPoint(new Vector3(0, cam.pixelHeight, cam.nearClipPlane));
+        GameObject rain = Instantiate(_rainPrefab);
+        rain.transform.position = position;
         _lastShowerAction = (ulong)System.DateTime.Now.Ticks;
         PlayerPrefs.SetString(LAST_SHOWER_KEY, _lastShowerAction.ToString());
         _titoMood.addMoodPoints(_showerPoints);
@@ -263,10 +361,12 @@ public class MoodActionsController : MonoBehaviour
         if (secondsLeft < 0)
         {
             _playText.text = "";
+            _playClock.gameObject.SetActive(false);
             return true;
         }
         else
         {
+            _playClock.gameObject.SetActive(true);
             return false;
         }
     }
@@ -279,10 +379,12 @@ public class MoodActionsController : MonoBehaviour
         if (secondsLeft < 0)
         {
             _eatText.text = "";
+            _eatClock.gameObject.SetActive(false);
             return true;
         }
         else
         {
+            _eatClock.gameObject.SetActive(true);
             return false;
         }
     }
@@ -295,10 +397,12 @@ public class MoodActionsController : MonoBehaviour
         if (secondsLeft < 0)
         {
             _showerText.text = "";
+            _showerClock.gameObject.SetActive(false);
             return true;
         }
         else
         {
+            _showerClock.gameObject.SetActive(true);
             return false;
         }
     }
